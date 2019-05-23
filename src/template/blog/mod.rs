7b367568d46491212dde85e643ee298447dd5c69
Stephan Goeppentin <dedda102@gmail.com>
2019-05-23 09:07:40 +0200

    removed separate modules for each blog

--- src/template/blog/mod.rs
+++ src/template/blog/mod.rs
@@ -1,34 +1,66 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
 use maud::Markup;
 use rocket::Rocket;
 
 use crate::data::models::Post;
 use crate::template::elements::Link;
 use crate::template::page;
+use crate::data::read::{load_posts, load_post};
+use crate::template::blog::post::render_post;
 
 pub mod post_renderable;
-pub mod moto;
 pub mod post;
-pub mod roggen;
-pub mod tech;
 
-pub fn embed_blog_contents(title: &Link, contents: Markup) -> Markup {
-    page(title, contents)
+lazy_static! {
+    static ref BLOGS: Arc<Mutex<HashMap<String, Blog>>> = {
+        let mut map = HashMap::new();
+        map.insert("moto".to_string(), Blog::for_name("Moto"));
+        map.insert("roggen".to_string(), Blog::for_name("Roggen"));
+        map.insert("tech".to_string(), Blog::for_name("Tech"));
+        Arc::new(Mutex::new(map))
+    };
 }
 
-pub fn blog_home(title: &Link, posts: &Vec<Post>) -> Markup {
-    embed_blog_contents(title, html! {
-        (posts_overview(posts))
-    })
+#[derive(Clone)]
+struct Blog {
+    name: String,
+    path: String,
+    title: Link,
+}
+
+impl Blog {
+    fn for_name<S>(name: S) -> Blog where S: ToString {
+        Blog {
+            name: name.to_string(),
+            path: name.to_string().to_ascii_lowercase(),
+            title: Link::for_blog_name(name),
+        }
+    }
 }
 
 pub fn mount_blogs(rocket: Rocket) -> Rocket {
-    let rocket = moto::mount(rocket);
-    let rocket = roggen::mount(rocket);
-    let rocket = tech::mount(rocket);
-    rocket.mount("/blog", routes![blog_list])
+    rocket.mount("/blog", routes![post, blog_main, blog_list])
 }
 
-pub fn posts_overview(posts: &Vec<Post>) -> Markup {
+fn get_blog<S>(name: S) -> Option<Blog> where S: ToString {
+    match BLOGS.lock().unwrap().get(&name.to_string()) {
+        Some(b) => Some(b.clone()),
+        None => None,
+    }
+}
+
+fn embed_blog_contents(title: &Link, contents: Markup) -> Markup {
+    page(title, contents)
+}
+
+fn blog_home(blog: Blog) -> Markup {
+    let posts = load_posts(&blog.path);
+    embed_blog_contents(&blog.title, posts_overview(&posts))
+}
+
+fn posts_overview(posts: &Vec<Post>) -> Markup {
     let items: Vec<Markup> = posts.iter().map(|p| post_overview_item(p)).collect();
     match posts.len() {
         0 => html! {
@@ -55,14 +87,42 @@ fn post_overview_item(post: &Post) -> Markup {
     }
 }
 
+fn blog_not_found() -> Markup {
+    html! {}
+}
+
 #[get("/")]
 fn blog_list() -> Markup {
     page(&Link::new("Blogs", "/blog"),
-        html! {
+         html! {
         ul class="blog-list" {
             li { a href="/blog/moto" { "Moto" } }
             li { a href="/blog/roggen" { "Roggen" } }
             li { a href="/blog/tech" { "Tech" } }
         }
     })
+}
+
+#[get("/<name>")]
+fn blog_main(name: String) -> Markup {
+    match get_blog(name) {
+        Some(b) => blog_home(b),
+        None => blog_not_found(),
+    }
+}
+
+#[get("/<blog>/<id>")]
+fn post(blog: String, id: i32) -> Markup {
+    match get_blog(blog) {
+        Some(b) => {
+            let contents = match load_post(id) {
+                Some(post) => render_post(&post),
+                None => html! {
+                    h1 { "Post not found!" }
+                }
+            };
+            embed_blog_contents(&b.title, contents)
+        },
+        None => blog_not_found(),
+    }
 }
--- src/template/elements.rs
+++ src/template/elements.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Link {
     pub title: String,
     pub href: String,
@@ -8,6 +9,13 @@ impl Link {
         Link {
             title: title.to_string(),
             href: href.to_string(),
+        }
+    }
+
+    pub fn for_blog_name<S>(name: S) -> Link where S: ToString {
+        Link {
+            title: name.to_string(),
+            href: format!("/blog/{}", name.to_string().to_ascii_lowercase()),
         }
     }
 }
