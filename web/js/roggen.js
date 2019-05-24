48147be8ecd12c317d7b76963e2ec46836134bbf
Stephan Goeppentin <dedda102@gmail.com>
2019-05-24 09:57:49 +0200

    read language from cookie in js

--- web/js/roggen.js
+++ web/js/roggen.js
@@ -8,11 +8,38 @@ Date.prototype.ddmmyyyy = function() {
     ].join('.')
 }
 
+var getCookie = function getCookie(cname) {
+    var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+};
+
+var getLanguage = function getLanguage() {
+    var lang = getCookie('selected-language');
+    return lang == '' ? 'en' : lang;
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelectorAll('[data-published]').forEach(function(element) {
         var time = element.attributes["data-published"].value;
         if (time == "") {
-            element.textContent = "Not published";
+            switch (getLanguage()) {
+                case 'de':
+                    element.textContent = "Nicht veröffentlicht";
+                    break;
+                default:
+                    element.textContent = "Not published";
+            }
         } else {
             time = new Date(time);
             element.textContent = time.ddmmyyyy();
